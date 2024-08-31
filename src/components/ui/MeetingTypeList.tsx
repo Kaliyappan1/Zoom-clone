23c4e33dbbde922cecc8b0a4b2528970c7fdd312
kaliyappan <yappank17@gmail.com>
2024-08-31 23:01:53 +0530

    start instant meeting model created

--- src/components/ui/MeetingTypeList.tsx
+++ src/components/ui/MeetingTypeList.tsx
@@ -3,11 +3,16 @@
 import React, { useState } from 'react'
 import HomeCard from './HomeCard'
 import { useRouter } from 'next/navigation'
+import MeetingModal from './MeetingModal'
 
 const MeetingTypeList = () => {
     const router = useRouter()
 
     const [meetingState, setMeetingState] = useState<'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined>()
+   const createMeeting =  ()=> {
+        
+   }
+   
     return (
         <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
             <HomeCard
@@ -35,8 +40,16 @@ const MeetingTypeList = () => {
                 img='/icons/join-meeting.svg'
                 title='Join Meeting'
                 description='Via invitation link'
-                handleClick={() => router.push('/recordings')}
+                handleClick={() => setMeetingState('isJoiningMeeting')}
                 className="bg-yellow-1"
+            />
+
+            <MeetingModal
+            isOpen={meetingState === 'isInstantMeeting'}
+            onClose={() => setMeetingState(undefined)}
+            title= "Start an Instant Meeting"
+            buttonText="Start Meeting"
+            handleClick={createMeeting}
             />
         </section>
     )
